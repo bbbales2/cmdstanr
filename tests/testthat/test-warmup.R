30c5c5ea9d2cd2c19221a23fdfa56f988d3e4ea5
Ben <bbbales2@gmail.com>
2019-11-19 13:55:12 -0500

    Updated test to work with latest merge

--- tests/testthat/test-warmup.R
+++ tests/testthat/test-warmup.R
@@ -4,15 +4,11 @@ NOT_CRAN <-
   identical(Sys.getenv("TRAVIS"), "true")
 
 if (NOT_CRAN) {
-  set_cmdstan_path("/home/bbales2/cmdstan-warmup")
+  set_cmdstan_path("/home/bbales2/warmup/cmdstan")
   stan_program <- test_path("resources", "stan", "kilpisjarvi.stan")
   mod <- cmdstan_model(stan_file = stan_program, compile = TRUE)
 
   data_file <- test_path("resources", "data", "kilpisjarvi.dat")
-}
-
-expect_sample_output <- function(object) {
-  testthat::expect_output(object, "Gradient evaluation took")
 }
 
 test_that("sample() method works with warmup arguments", {
